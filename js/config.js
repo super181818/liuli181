a8b1d1a1b3a2dc337f3329dcc74184a69c790bff
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-20 20:25:29 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/*-------config start-------*/
+var _wip = '119.91.26.181';  
+var _port = '1725';  
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10000';
+
+
+/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/cwoss.js?v=' + _tt + '"> <\/script>');*/
